7092fd924ae16af9bbdc348900d290cb5fcf386d
Ghulam Muhiuddin <muhiuddin@thetowertech.com>
2026-05-03 02:15:47 +0500

    update docker file

--- src/main/java/com/step/nova/security/SecurityConfig.java
+++ src/main/java/com/step/nova/security/SecurityConfig.java
@@ -6,12 +6,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableMethodSecurity
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig 
 {
